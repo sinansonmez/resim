57b167448fdbd1ea6a5c285707105273393291b4
Sinan Sonmez (Chaush) <sinansonmez@outlook.com>
2023-10-26 20:56:31 +0000

    add more functionality to web page

--- react/index.jsx
+++ react/index.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import * as wasm from "hello-wasm-pack";
 import { createRoot } from 'react-dom/client';
+import cat from './asset/cat.jpg'
 
 const Component = () => {
-    return <div onClick={wasm.greet} >Hello from React!</div>;
+    return (
+        <>
+            <div style={{ display: 'flex' }} >
+                <button onClick={() => wasm.greet('sinan')} >Hello from React!</button>
+            </div>
+            <div>
+                <img src={cat} width={250} />
+            </div>
+        </>
+    );
 }
 
 // Render your React component instead
 const root = createRoot(document.getElementById('app'));
-root.render(<Component/>);
+root.render(<Component />);
--- react/webpack.config.js
+++ react/webpack.config.js
@@ -22,7 +22,15 @@ module.exports = {
             presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
-      }
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ]
+  },
+  experiments: {
+    syncWebAssembly: true,
+    asyncWebAssembly: true,
   }
 };
