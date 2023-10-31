dbc8c6ddc9eb13abccaee8b2a7146f13c6b1a4c5
Sinan Sonmez (Chaush) <sinansonmez@outlook.com>
2023-10-31 23:55:27 +0000

    try to load images from byte array

--- react/index.jsx
+++ react/index.jsx
@@ -2,20 +2,56 @@ import React, { useEffect } from 'react';
 import * as wasm from "hello-wasm-pack";
 import { createRoot } from 'react-dom/client';
 import cat from './asset/cat.jpg'
+import dice from './asset/dice.png'
 
 const Component = () => {
+
+    function loadImageAsUint8Array(imagePath, callback) {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', imagePath, true);
+        xhr.responseType = 'arraybuffer';
+
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                const arrayBuffer = xhr.response;
+                const uint8Array = new Uint8Array(arrayBuffer);
+                callback(uint8Array);
+            } else {
+                console.error('Failed to load the image.');
+                callback(null);
+            }
+        };
+
+        xhr.send();
+    }
+
     const onClickHandler = (imageUrl) => {
         console.time('start')
-        wasm.greet(imageUrl)
+        loadImageAsUint8Array(imageUrl, function (uint8Array) {
+            if (uint8Array) {
+                console.log('Uint8Array:', uint8Array);
+                const image = wasm.greet(uint8Array)
+                // Create a Blob from the Uint8Array
+                const blob = new Blob([image], { type: 'image/png' });
+
+                // Create a Data URL from the Blob
+                const url = URL.createObjectURL(blob);
+                document.getElementById('new').src = url;
+                console.log("new image: ", image);
+            } else {
+                console.err('Error loading the image.');
+            }
+        });
         console.timeEnd('start')
     }
     return (
         <>
             <div style={{ display: 'flex' }} >
-                <button onClick={() => onClickHandler(cat)} >Hello from React!</button>
+                <button onClick={() => onClickHandler(dice)} >Hello from React!</button>
             </div>
             <div>
-                <img src={cat} width={250} />
+                <img src={dice} width={250} />
+                <img id='new' src={cat} width={250} />
             </div>
         </>
     );
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,5 +1,7 @@
 mod utils;
 
+use std::borrow::Borrow;
+
 use wasm_bindgen::prelude::*;
 use image::{open, ImageFormat};
 use web_sys::console;
@@ -17,27 +19,22 @@ extern {
 }
 
 #[wasm_bindgen]
-pub fn greet(path: &str) -> Result<(), JsValue> {
-        // Open the image
-        let img = match open(path) {
-            Ok(image) => image,
-            Err(err) => {
-                console::error_1(&format!("Error opening image: {}", err).into());
-                return Err(Error::new(&format!("Error opening image: {}", err)).into());
-            }
-        };
-    
-        // Convert to grayscale
-        let grayscale_img = img.grayscale();
-    
-        // Save the grayscale image
-        let output_path = "output.png"; // Change the output path as needed
-        match grayscale_img.save_with_format(output_path, ImageFormat::Png) {
-            Ok(_) => Ok(()),
-            Err(err) => {
-                console::error_1(&format!("Error saving image: {}", err.to_string()).into());
-                Err(Error::new(&format!("Error saving image: {}", err.to_string())).into())
-            }
+pub fn greet(bytes: Vec<u8>) -> Vec<u8> {
+    console_error_panic_hook::set_once();
+
+    console::log_1(&JsValue::from_str(format!("converting from bytes: {:?}", bytes).as_str()));
+  
+    let img = image::load_from_memory_with_format(&bytes, image::ImageFormat::Png);
+
+    match img {
+        Ok(loaded_img) => {
+            console::log_1(&JsValue::from_str(format!("loaded image: {:?}", loaded_img).as_str()));
+            loaded_img.to_luma8().into_vec()
+        },
+        Err(err) => {
+            console::error_1(&JsValue::from_str(format!("converting from bytes: {:?}", err).as_str()));
+            Vec::new()
         }
+    }
     // alert(&format!("Hello, {}!", path));
 }
