3153f882cf170eaf9af57369c48e1bed4fe383ee
Sinan Sonmez (Chaush) <sinansonmez@outlook.com>
2023-11-01 00:01:55 +0000

    change the loaf to convert it uint8array

--- react/index.jsx
+++ react/index.jsx
@@ -6,42 +6,37 @@ import dice from './asset/dice.png'
 
 const Component = () => {
 
-    function loadImageAsUint8Array(imagePath, callback) {
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', imagePath, true);
-        xhr.responseType = 'arraybuffer';
-
-        xhr.onload = function () {
-            if (xhr.status === 200) {
-                const arrayBuffer = xhr.response;
-                const uint8Array = new Uint8Array(arrayBuffer);
-                callback(uint8Array);
-            } else {
-                console.error('Failed to load the image.');
-                callback(null);
+    async function loadImageAsUint8Array(imagePath) {
+        try {
+            const response = await fetch(imagePath);
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
             }
-        };
-
-        xhr.send();
+            const arrayBuffer = await response.arrayBuffer();
+            // Create a Uint8Array from the array buffer
+            const uint8Array = new Uint8Array(arrayBuffer);
+            return uint8Array;
+        } catch (error) {
+            console.error('Error:', error);
+        }
     }
 
-    const onClickHandler = (imageUrl) => {
+    const onClickHandler = async (imageUrl) => {
         console.time('start')
-        loadImageAsUint8Array(imageUrl, function (uint8Array) {
-            if (uint8Array) {
-                console.log('Uint8Array:', uint8Array);
-                const image = wasm.greet(uint8Array)
-                // Create a Blob from the Uint8Array
-                const blob = new Blob([image], { type: 'image/png' });
+        const uint8Array = await loadImageAsUint8Array(imageUrl)
+        if (uint8Array) {
+            console.log('Uint8Array:', uint8Array);
+            const image = wasm.greet(uint8Array)
+            // Create a Blob from the Uint8Array
+            const blob = new Blob([image], { type: 'image/png' });
 
-                // Create a Data URL from the Blob
-                const url = URL.createObjectURL(blob);
-                document.getElementById('new').src = url;
-                console.log("new image: ", image);
-            } else {
-                console.err('Error loading the image.');
-            }
-        });
+            // Create a Data URL from the Blob
+            const url = URL.createObjectURL(blob);
+            document.getElementById('new').src = url;
+            console.log("new image: ", image);
+        } else {
+            console.err('Error loading the image.');
+        }
         console.timeEnd('start')
     }
     return (
