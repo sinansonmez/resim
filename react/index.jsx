caf9d6ea8644d9ae92d516a33172b10b45f73eec
Sinan Sonmez (Chaush) <sinansonmez@outlook.com>
2023-11-05 15:13:35 +0000

    clean up the code

--- react/index.jsx
+++ react/index.jsx
@@ -5,7 +5,6 @@ import cat from './asset/cat.jpg'
 import dice from './asset/dice.png'
 
 const Component = () => {
-
     // Function to convert an image to a Base64 encoded string
     function getBase64Image(imagePath, callback) {
         const reader = new FileReader();
@@ -34,7 +33,7 @@ const Component = () => {
         console.time('start')
         getBase64Image(imageUrl, (base64) => {
             if (base64) {
-                let data = base64.replace(/^data:image\/(png|jpg);base64,/, "");
+                let data = base64.replace(/^data:image\/(png|jpeg);base64,/, "");
                 console.log('Uint8Array:', base64);
                 const image = wasm.greet(data)
                 // Create a Blob from the Uint8Array
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,21 +1,12 @@
 mod utils;
 
-use base64::{
-    engine::general_purpose,
-    Engine,
-};
-use image::{DynamicImage, ImageFormat, io::Reader};
+use base64::{engine::general_purpose, Engine};
+use image::{io::Reader, DynamicImage, ImageFormat};
+use std::fs::File;
+use std::io::prelude::*;
 use std::io::Cursor;
 use wasm_bindgen::prelude::*;
 use web_sys::console;
-use std::fs::File;
-use std::io::prelude::*;
-
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
 extern "C" {
@@ -38,23 +29,31 @@ pub fn base64_to_vec(base64: &str) -> Vec<u8> {
 
 pub fn dyn_image_from_raw(image: Vec<u8>) -> DynamicImage {
     // convert a vec of raw pixels (as u8s) to a DynamicImage type
-    let img_buffer = image::ImageBuffer::from_vec(
-        600,
-        800,
-        image,
-    )
-    .unwrap();
+    let img_buffer = image::ImageBuffer::from_vec(600, 800, image).unwrap();
     DynamicImage::ImageRgba8(img_buffer)
 }
 
-fn create_image_from_bytes(data: Vec<u8>) -> Result<DynamicImage, image::ImageError> {
+fn create_image_from_bytes(data: Vec<u8>) -> DynamicImage {
     // Create a Cursor from the Vec<u8>
     let cursor = Cursor::new(data);
 
     // Attempt to read the image from the cursor
-    let dynamic_image = Reader::new(cursor).with_guessed_format().unwrap().decode()?;
+    let dynamic_image = Reader::with_format(cursor, ImageFormat::Png).decode();
 
-    Ok(dynamic_image)
+    match dynamic_image {
+        Ok(dynamic_image_success) => {
+            console::error_1(&JsValue::from_str(
+                format!("dynamic_image_success: {:?}", dynamic_image_success).as_str(),
+            ));
+            dynamic_image_success
+        }
+        Err(dynamic_image_err) => {
+            console::error_1(&JsValue::from_str(
+                format!("dynamic_image_err: {:?}", dynamic_image_err).as_str(),
+            ));
+            DynamicImage::new_luma8(32, 32)
+        }
+    }
 }
 
 fn image_to_base64(image: DynamicImage, format: ImageFormat) -> String {
@@ -79,30 +78,18 @@ fn image_to_base64(image: DynamicImage, format: ImageFormat) -> String {
 pub fn get_base64(image: Vec<u8>) -> String {
     let img = create_image_from_bytes(image);
 
-    let dynamic_image = match img {
-        Ok(img_base) => {
-            let base64 = image_to_base64(img_base, image::ImageFormat::Png);
-            let res_base64 = format!("data:image/png;base64,{}", base64.replace("\r\n", ""));
-            res_base64
-        }
-        Err(img_base_err) => {
-            console::error_1(&JsValue::from_str(format!("image base 64 err: {:?}", img_base_err).as_str()));
-            String::new()
-        }
-    };
-    dynamic_image
+    let base64 = image_to_base64(img, image::ImageFormat::Png);
+    let res_base64 = format!("data:image/png;base64,{}", base64.replace("\r\n", ""));
+    res_base64
 }
 
 #[wasm_bindgen]
 pub fn greet(base64: &str) -> String {
-    console_error_panic_hook::set_once();
     let base64_to_vec: Vec<u8> = base64_to_vec(base64);
 
     let slice = base64_to_vec.as_slice();
 
     let mut img = image::load_from_memory(slice).unwrap();
     img = DynamicImage::ImageLuma8(img.to_luma8());
-    get_base64(img.to_bytes())
-
-    // alert(&format!("Hello, {}!", path));
+    get_base64(img.into_bytes())
 }
