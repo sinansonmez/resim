6348aef15a56c1929cbf49ed1fdf137dfcd04338
Sinan Sonmez (Chaush) <sinansonmez@outlook.com>
2023-11-05 21:52:25 +0000

    return a correct image

--- react/index.jsx
+++ react/index.jsx
@@ -1,65 +1,70 @@
 import React, { useEffect } from 'react';
 import * as wasm from "hello-wasm-pack";
 import { createRoot } from 'react-dom/client';
-import cat from './asset/cat.jpg'
 import dice from './asset/dice.png'
 
 const Component = () => {
-    // Function to convert an image to a Base64 encoded string
-    function getBase64Image(imagePath, callback) {
-        const reader = new FileReader();
-      
-        reader.onload = function () {
-          if (typeof callback === 'function') {
-            callback(reader.result);
-          }
-        };
-      
-        // Assuming imagePath is a valid local path to the image file
-        fetch(imagePath)
-          .then((response) => response.blob())
-          .then((blob) => {
-            reader.readAsDataURL(blob);
-          })
-          .catch((error) => {
-            console.error('Error loading image:', error);
-            if (typeof callback === 'function') {
-              callback(null);
-            }
-          });
-      }
+  // Function to convert an image to a Base64 encoded string
+  function getBase64Image(imagePath, callback) {
+    const reader = new FileReader();
 
-    const onClickHandler = async (imageUrl) => {
-        console.time('start')
-        getBase64Image(imageUrl, (base64) => {
-            if (base64) {
-                let data = base64.replace(/^data:image\/(png|jpeg);base64,/, "");
-                console.log('Uint8Array:', base64);
-                const image = wasm.greet(data)
-                // Create a Blob from the Uint8Array
-                const blob = new Blob([image], { type: 'image/png' });
-    
-                // Create a Data URL from the Blob
-                const url = URL.createObjectURL(blob);
-                document.getElementById('new').src = url;
-                console.log("new image: ", image);
-            } else {
-                console.err('Error loading the image.');
-            }
-        })
-        console.timeEnd('start')
-    }
-    return (
-        <>
-            <div style={{ display: 'flex' }} >
-                <button onClick={() => onClickHandler(dice)} >Hello from React!</button>
-            </div>
-            <div>
-                <img src={dice} width={250} />
-                <img id='new' src={cat} width={250} />
-            </div>
-        </>
-    );
+    reader.onload = function () {
+      if (typeof callback === 'function') {
+        callback(reader.result);
+      }
+    };
+
+    // Assuming imagePath is a valid local path to the image file
+    fetch(imagePath)
+      .then((response) => response.blob())
+      .then((blob) => {
+        reader.readAsDataURL(blob);
+      })
+      .catch((error) => {
+        console.error('Error loading image:', error);
+        if (typeof callback === 'function') {
+          callback(null);
+        }
+      });
+  }
+
+  const onClickHandler = async (imageUrl) => {
+    console.time('start')
+    getBase64Image(imageUrl, (base64) => {
+      if (base64) {
+        let data = base64.replace(/^data:image\/(png|jpeg);base64,/, "");
+        const image = wasm.grayscale(data)
+        // Create a canvas element
+        const canvas = document.createElement('canvas');
+
+        // Set the canvas dimensions to match the ImageData's dimensions
+        canvas.width = image.width;
+        canvas.height = image.height;
+
+        // Get the 2D rendering context
+        const ctx = canvas.getContext('2d');
+
+        // Use putImageData to render the ImageData
+        ctx.putImageData(image, 0, 0);
+
+        // Append the canvas to the DOM or do other operations
+        document.body.appendChild(canvas);
+      } else {
+        console.err('Error loading the image.');
+      }
+    })
+    console.timeEnd('start')
+  }
+  return (
+    <>
+      <div style={{ display: 'flex' }} >
+        <button onClick={() => onClickHandler(dice)} >Hello from React!</button>
+      </div>
+      <div>
+        <img src={dice} width={250} />
+      </div>
+    </>
+  );
 }
 
 // Render your React component instead
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,16 +1,30 @@
 mod utils;
 
 use base64::{engine::general_purpose, Engine};
-use image::{io::Reader, DynamicImage, ImageFormat};
-use std::fs::File;
-use std::io::prelude::*;
-use std::io::Cursor;
+use image::DynamicImage;
 use wasm_bindgen::prelude::*;
-use web_sys::console;
+use wasm_bindgen::Clamped;
+use web_sys::{console, ImageData};
 
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
+pub fn to_image_data(mut raw_pixels: Vec<u8>, width: u32, height: u32) -> ImageData {
+    // Check if the dimensions are valid
+    if width <= 0 || height <= 0 {
+        console::error_1(&JsValue::from_str("Invalid image dimensions"));
+        // Provide a fallback ImageData with valid dimensions if an error occurs
+        return ImageData::new_with_sw(1, 1).unwrap();
+    }
+
+    let image_data =
+        ImageData::new_with_u8_clamped_array_and_sh(Clamped(&mut raw_pixels), width, height);
+    match image_data {
+        Ok(image_data_success) => image_data_success,
+        Err(image_data_err) => {
+            console::error_1(&JsValue::from_str(
+                format!("image_data_err {:?}", image_data_err).as_str(),
+            ));
+            ImageData::new_with_sw(width, height).unwrap()
+        }
+    }
 }
 
 pub fn base64_to_vec(base64: &str) -> Vec<u8> {
@@ -27,69 +41,17 @@ pub fn base64_to_vec(base64: &str) -> Vec<u8> {
     }
 }
 
-pub fn dyn_image_from_raw(image: Vec<u8>) -> DynamicImage {
-    // convert a vec of raw pixels (as u8s) to a DynamicImage type
-    let img_buffer = image::ImageBuffer::from_vec(600, 800, image).unwrap();
-    DynamicImage::ImageRgba8(img_buffer)
-}
+fn vec_to_image(vec: Vec<u8>) -> DynamicImage {
+    let slice = vec.as_slice();
 
-fn create_image_from_bytes(data: Vec<u8>) -> DynamicImage {
-    // Create a Cursor from the Vec<u8>
-    let cursor = Cursor::new(data);
-
-    // Attempt to read the image from the cursor
-    let dynamic_image = Reader::with_format(cursor, ImageFormat::Png).decode();
-
-    match dynamic_image {
-        Ok(dynamic_image_success) => {
-            console::error_1(&JsValue::from_str(
-                format!("dynamic_image_success: {:?}", dynamic_image_success).as_str(),
-            ));
-            dynamic_image_success
-        }
-        Err(dynamic_image_err) => {
-            console::error_1(&JsValue::from_str(
-                format!("dynamic_image_err: {:?}", dynamic_image_err).as_str(),
-            ));
-            DynamicImage::new_luma8(32, 32)
-        }
-    }
-}
-
-fn image_to_base64(image: DynamicImage, format: ImageFormat) -> String {
-    // Create a temporary file
-    let mut temp_file = File::create("temp_image.png").unwrap();
-
-    // Write the image data to the temporary file
-    image.write_to(&mut temp_file, format);
-
-    // Read the image data from the temporary file
-    let mut temp_file = File::open("temp_image.png").unwrap();
-    let mut buffer = Vec::new();
-    temp_file.read_to_end(&mut buffer);
-
-    // Encode the image data to base64
-    let base64_encoded = base64::encode(&buffer);
-
-    base64_encoded
-}
-
-/// Convert the PhotonImage to base64.
-pub fn get_base64(image: Vec<u8>) -> String {
-    let img = create_image_from_bytes(image);
-
-    let base64 = image_to_base64(img, image::ImageFormat::Png);
-    let res_base64 = format!("data:image/png;base64,{}", base64.replace("\r\n", ""));
-    res_base64
+    let img = image::load_from_memory(slice).unwrap();
+    DynamicImage::ImageLuma8(img.to_luma8())
 }
 
 #[wasm_bindgen]
-pub fn greet(base64: &str) -> String {
+pub fn grayscale(base64: &str) -> ImageData {
     let base64_to_vec: Vec<u8> = base64_to_vec(base64);
-
-    let slice = base64_to_vec.as_slice();
-
-    let mut img = image::load_from_memory(slice).unwrap();
-    img = DynamicImage::ImageLuma8(img.to_luma8());
-    get_base64(img.into_bytes())
+    let img = vec_to_image(base64_to_vec);
+    // todo why width and height of img is not correct
+    to_image_data(img.as_bytes().to_vec(), 400, 300)
 }
